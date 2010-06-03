4d26a11b4d99d99481eeb59faf184633e826ec03
Gabriel Tarnovan <gabriel.tarnovan@cubus.ro>
2010-06-03 17:55:52 +0300

    * added migration generator

---
+++ generators/simple_audit_migration/simple_audit_migration_generator.rb
@@ -0,0 +1,8 @@
+class SimpleAuditMigrationGenerator < Rails::Generator::NamedBase
+  def manifest
+    record do |m|
+      m.migration_template 'migration.rb', "db/migrate"
+    end
+  end
+
+end
---
+++ generators/simple_audit_migration/templates/migration.rb
@@ -0,0 +1,22 @@
+class CreateAudits < ActiveRecord::Migration
+  def self.up
+    create_table :audits do |t|
+      t.belongs_to :auditable,  :polymorphic => true
+      t.belongs_to :user,       :polymorphic => true
+
+      t.string :username
+      t.string :action
+      t.text   :changes
+      t.timestamps
+
+    end
+
+    add_index :audits, [:auditable_id, :auditable_type], :name => 'auditable_index'
+    add_index :audits, [:user_id, :user_type], :name => 'user_index'
+    add_index :audits, :created_at
+  end
+
+  def self.down
+    drop_table :audits
+  end
+end
